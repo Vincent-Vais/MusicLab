09d98cb12ddea6b6c7b307920589bb32abbe1aa9
Vincent-Vais <vincentvais17@gmail.com>
2020-11-08 21:34:52 -0600

    removed dev tools from production

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,15 @@ import reducer from "./reducer";
 
 const middleware = [thunk];
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+let store;
+
+if (process.env.NODE_ENV !== "production") {
+  store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+  );
+} else {
+  store = createStore(reducer, applyMiddleware(...middleware));
+}
 
 export default store;
